48bebbafee1caeaea20f4ac49f28f61a12b78642
Alex King <alexking124@gmail.com>
2018-01-14 15:40:12 -0800

    Initial Commit

---
+++ nhl-scores/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nhl-scores
+//
+//  Created by Alex King on 1/14/18.
+//  Copyright © 2018 Alex King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
