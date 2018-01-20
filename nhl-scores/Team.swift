e841adb7b386cd8c5d269bd222d7556eab767def
Alex King <alexking124@gmail.com>
2018-01-19 21:38:26 -0800

    Moved TeamID enum to separate file

--- nhl-scores/Team.swift
+++ nhl-scores/Team.swift
@@ -10,46 +10,15 @@ import Foundation
 import UIKit
 import RealmSwift
 
-enum NHLTeamID: Int {
-    case nhl = 0
-    case njd = 1
-    case nyi = 2
-    case nyr = 3
-    case phi = 4
-    case pit = 5
-    case bos = 6
-    case buf = 7
-    case mtl = 8
-    case ott = 9
-    case tor = 10
-    case car = 12
-    case fla = 13
-    case tbl = 14
-    case wsh = 15
-    case chi = 16
-    case det = 17
-    case nsh = 18
-    case stl = 19
-    case cgy = 20
-    case col = 21
-    case edm = 22
-    case van = 23
-    case ana = 24
-    case dal = 25
-    case lak = 26
-    case sjs = 28
-    case cbj = 29
-    case min = 30
-    case wpg = 52
-    case ari = 53
-    case vgk = 54
-}
-
 class Team: Object {
     
     @objc dynamic var rawId: Int = 0
     @objc dynamic var teamName: String = ""
     @objc dynamic var locationName: String = ""
+    
+    override static func primaryKey() -> String? {
+        return "rawId"
+    }
     
     var id: NHLTeamID {
         return NHLTeamID(rawValue: rawId) ?? .nhl
