4bf6d9ced4aa7fe75e2c37d2f6073d4fd97eb84b
Alex King <alexking124@gmail.com>
2018-02-03 12:54:48 -0800

    Adding enum to keep track of sections

--- nhl-scores/View/Standings/StandingsViewController.swift
+++ nhl-scores/View/Standings/StandingsViewController.swift
@@ -11,12 +11,24 @@ import UIKit
 
 class StandingsViewController: UITableViewController {
     
+    private enum WildCardSection: Int {
+        case unknown = -1
+        case eastern = 0
+        case atlantic = 1
+        case metropolitan = 2
+        case easternWildCard = 3
+        case western = 4
+        case central = 5
+        case pacific = 6
+        case westernWildCard = 7
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Standings"
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "StandingsCell")
         tableView.register(StandingsConferenceHeaderView.self, forHeaderFooterViewReuseIdentifier: "ConferenceHeader")
     }
     
@@ -39,17 +51,31 @@ extension StandingsViewController {
         case 6:
             return 3 // Division Leaders
         case 3:
-            return 8 // Eastern Conference Wild Card
+            return 10 // Eastern Conference Wild Card
         case 7:
-            return 7 // Western Conference Wild Card
+            return 9 // Western Conference Wild Card
         default:
             return 0
         }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = String("\(indexPath.section) - \(indexPath.row)")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StandingsCell", for: indexPath)
+        
+        let sectionType = WildCardSection(rawValue: indexPath.section) ?? .unknown
+        switch sectionType {
+        case .atlantic: fallthrough
+        case .metropolitan: fallthrough
+        case .central: fallthrough
+        case .pacific:
+            cell.textLabel?.text = String("\(indexPath.section) - \(indexPath.row)")
+        case .easternWildCard: fallthrough
+        case .westernWildCard:
+            cell.textLabel?.text = "Wild Card"
+        default:
+            assertionFailure("Invalid section")
+            return UITableViewCell()
+        }
         return cell
     }
     
@@ -67,15 +93,11 @@ extension StandingsViewController {
         case 1: fallthrough
         case 2: fallthrough
         case 5: fallthrough
-//        case 6:
-////            headerView.height(18) // Division Leaders
-//        case 3:
-////            headerView.height(18) // Eastern Conference Wild Card
-//        case 7:
-////            headerView.height(18) // Western Conference Wild Card
+        case 6: fallthrough
+        case 3: fallthrough
+        case 7: fallthrough
         default:
             break
-//            headerView.height(0)
         }
         return headerView
     }
